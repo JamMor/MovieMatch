bb4b3f77fe1683320ec3eb63d21a51b67b948080
JamMor <JamMor@gmail.com>
2022-02-20 01:27:06 -0800

    Added temp and shared list models

--- display_app/models.py
+++ display_app/models.py
@@ -13,19 +13,34 @@ class Movie(models.Model):
     def __str__(self):
         return self.title
 
-class MovieList(models.Model):
+class SavedMovieList(models.Model):
     list_name = models.CharField(max_length=255, null=True)
-    created_by = models.ForeignKey('app_login_and_reg.User', related_name="created_lists", on_delete = models.CASCADE, null=True)
-    movies = models.ManyToManyField(Movie, related_name="in_lists")
+    created_by = models.ForeignKey('app_login_and_reg.User', related_name="saved_lists", on_delete = models.CASCADE, null=True)
+    movies = models.ManyToManyField(Movie, related_name="in_saved_lists")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-class SharedList(models.Model):
-    users = models.TextField()
-    movies = models.ManyToManyField(Movie, related_name="in_shared_lists")
-    chosen = models.TextField()
+class TempMovieList(models.Model):
+    # created_by will have a base 58 encoded uuid
+    created_by = models.CharField(max_length=255, null=True)
+    movies = models.ManyToManyField(Movie, related_name="in_temp_lists")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class SharedMovieList(models.Model):
+    # users will have a list of base 58 encoded uuid
+    users = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class SharedMovie(models.Model):
+    # submitted_by will be a list of base 58 encoded uuids
+    submitted_by = models.TextField()
+    shared_list = models.ForeignKey(SharedMovieList, related_name="shared_movies", on_delete = models.CASCADE, null=True)
+    movie = models.ForeignKey(Movie, related_name="shared_movies", on_delete = models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.movie.title
 
 # class Friends(models.Model):
 #     user = models.OneToOneField(User, on_delete=models.CASCADE)
