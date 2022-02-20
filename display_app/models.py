778aafbf1659b4b27ca99970ee1dea036807e2ab
JamMor <JamMor@gmail.com>
2022-02-20 04:37:11 -0800

    Added sharecode and json encoding methods

--- display_app/models.py
+++ display_app/models.py
@@ -1,3 +1,6 @@
+from enum import unique
+import json
+import shortuuid
 from django.db import models
 
 # Create your models here.
@@ -21,23 +24,34 @@ class SavedMovieList(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class TempMovieList(models.Model):
-    # created_by will have a base 58 encoded uuid
+    # created_by will have a base 57 encoded uuid
     created_by = models.CharField(max_length=255, null=True)
     movies = models.ManyToManyField(Movie, related_name="in_temp_lists")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class SharedMovieList(models.Model):
-    # users will have a list of base 58 encoded uuid
-    users = models.TextField()
+    sharecode = models.CharField(max_length=255, unique=True, default=shortuuid.uuid()[:8])
+    # users will have a list of base 57 encoded uuid
+    users = models.TextField(default='[]')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def set_users(self, x):
+        self.users = json.dumps(x)
+    def get_users(self):
+        return json.loads(self.users)
 
 class SharedMovie(models.Model):
-    # submitted_by will be a list of base 58 encoded uuids
-    submitted_by = models.TextField()
+    # submitted_by will be a list of base 57 encoded uuids
+    submitted_by = models.TextField(default='[]')
     shared_list = models.ForeignKey(SharedMovieList, related_name="shared_movies", on_delete = models.CASCADE, null=True)
     movie = models.ForeignKey(Movie, related_name="shared_movies", on_delete = models.CASCADE, null=True)
+
+    def set_submitted_by(self, x):
+        self.submitted_by = json.dumps(x)
+    def get_submitted_by(self):
+        return json.loads(self.submitted_by)
 
     def __str__(self):
         return self.movie.title
