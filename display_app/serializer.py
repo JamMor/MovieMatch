36b7e0bf5b72d85b8eb2f23493c251a884f0e6d4
JamMor <JamMor@gmail.com>
2022-02-25 05:10:17 -0800

    Convert model to dict for json serializing

---
+++ display_app/serializer.py
@@ -0,0 +1,26 @@
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from .models import SharedMovieList, SharedMovie, UserUUID, Movie
+from django.db.models import Prefetch
+
+def SharedListEncoder(sharecode):
+    shared_list = SharedMovieList.objects.prefetch_related(
+                Prefetch('users', to_attr='pre_users'), 
+                Prefetch('shared_movies', to_attr='pre_shared_movies'), 
+                Prefetch('pre_shared_movies__submitted_by', to_attr='pre_submitted_by')).get(sharecode = sharecode)
+    
+    user_list = list(user.uuid for user in shared_list.pre_users)
+    
+    movie_list = []
+    for shared_movie in shared_list.pre_shared_movies:
+        movie_info = shared_movie.movie.__dict__
+        del movie_info['_state']
+        movie_info['submitted_by'] = list(user.uuid for user in shared_movie.pre_submitted_by)
+        movie_list.append(movie_info)
+
+    json_dict = {
+        'user_list' : user_list, 
+        'movie_list' : movie_list
+        }
+
+    return json_dict
