74da7750683d3acf65994de913d944b04c8655ee
JamMor <JamMor@gmail.com>
2022-02-26 01:24:39 -0800

    Converted to JsonSockets with working sample logic

--- display_app/consumers.py
+++ display_app/consumers.py
@@ -1,10 +1,16 @@
 # chat/consumers.py
 import json
 from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
 
-class MatchConsumer(WebsocketConsumer):
+from display_app.models import SharedMovieList
+from .serializer import SharedListEncoder
+
+class MatchConsumer(JsonWebsocketConsumer):
     def connect(self):
+        
         self.sharecode = self.scope['url_route']['kwargs']['sharecode']
         self.match_group_name = 'match_%s' % self.sharecode
 
@@ -24,25 +30,60 @@ class MatchConsumer(WebsocketConsumer):
         )
 
     # Receive message from WebSocket Client
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print(message)
+    def receive_json(self, content):
+        print(content)
+        command = content['command']
+        user_uuid = self.scope["session"]["uuid"]
 
-        # Send message to ChannelLayer
-        async_to_sync(self.channel_layer.group_send)(
-            self.match_group_name,
-            {
-                'type': 'match_message',
-                'message': "Message returned: " + message
+        if command == 'eliminate':
+            movie_id = content['movie_id']
+            #Eliminate movie
+            #----logic----
+
+            #Confirm Removal for Group
+            send_content = {
+            'command': 'eliminate',
+            'status' : "success",
+            'message': f'Movie ID {movie_id} eliminated by {user_uuid}.'
             }
-        )
+
+            async_to_sync(self.channel_layer.group_send)(
+                self.match_group_name,
+                {
+                    'type': 'eliminate_message',
+                    'content': send_content
+                }
+            )
+
+        elif command == 'initialize':
+            print("Sharecode from Receive: " + self.sharecode)
+            model_dict = SharedListEncoder(self.sharecode)
+
+            self.send_json({
+                'command': 'initialize',
+                'status' : 'success',
+                'share_list': model_dict
+            })
+        
+        else:
+            self.send_json({
+                'command': command,
+                'status' : 'failed',
+                'error_message' : "Command not found."
+            })
 
     # Receive message from ChannelLayer
-    def match_message(self, event):
-        message = event['message']
+    def eliminate_message(self, event):
+        content = event['content']
 
         # Send message to WebSocket Client
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send_json(content)
+
+    #Custom JSON coders (for dates)
+    @classmethod
+    def decode_json(cls, text_data):
+        return json.loads(text_data)
+
+    @classmethod
+    def encode_json(cls, content):
+        return json.dumps(content, cls=DjangoJSONEncoder)
