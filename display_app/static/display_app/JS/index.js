355f3cac0f24d5f3a2cf06dc4659bc2687c5e5e6
JamMor <JamMor@gmail.com>
2022-03-11 02:05:50 -0800

    Updated handler to add to DOM. Use template function

--- display_app/static/display_app/JS/index.js
+++ display_app/static/display_app/JS/index.js
@@ -106,48 +106,29 @@ $(document).ready(function() {
     }, 1000));
 
     //Handler to add movie to list and dom
+    $('.carousel').on("click", "a.add-btn", function () {
+        // Get Movie ID from btn ID ("search-movie_XXXXXXX")
+        let thisId = this.id.slice(13);
+        console.log(`Adding movie id ${thisId}`);
+        let thisMovie;
+        //If movie not already in list, get info from search results and push to list
+        if(!movie_list.some(movie => movie.id == thisId)){
+            for(let i=0; i<search_results.length; i++){
+                if(search_results[i].id == thisId){
+                    thisMovie = search_results[i];
+                    break
+                }
+            }
+            console.log(thisMovie.title)
+            movie_list.push(thisMovie);
 
-    //Autocomplete that pulls dynamic data from API. Also adds selected elements 
-    //info to an object to send back to server on upload.
-    $( "#searchbar" ).autocomplete({
-        appendTo: "#searchbox",
-        delay: 500,
-        minLength:2,
-        source: function(request, response) {
-            var search_str = request.term;
-            $.get(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${search_str}`,
-                function(data){
-                    response( data.results );
-                    return
-                }, "json")
-        },
-        select: function (event, movie_item){
-            let movie = movie_item.item;
-            console.log(movie);
-            if (movie_list.some(each_movie => each_movie.id == movie.id)) {
-                console.log("It's already in here dumdum.")
-            }
-            //Adds movie to DOM
-            else {
-                movie_list.push(movie);
-                background_img = (movie.poster_path == null) 
-                    ? "style='background-color:red'"
-                    : `style='background-image: url(${image_link}w154${movie.poster_path})'`
-        
-                $('#movie_list')
-                    .append(
-                        `<div class='list_item personal' ${background_img}>\
-                        <h5>${movie.title} - ${movie.release_date.slice(0, 4)}</h5>\
-                        </div>`);
-            }
-            
-        },
-    }).data('ui-autocomplete')._renderItem = function(ul, movie) {
-        return $( "<li>" )
-        .attr( "item.autocomplete", movie )
-        .append( `<a>${movie.title} - ${movie.release_date}<img src='${image_link}w92${movie.poster_path}'></a>` )
-        .appendTo( ul );
-    }
+            //Add movie to DOM
+            $("#movie_list").append(MovieCard("movie", thisMovie));
+        }
+        else{
+            console.log("Already added.")
+        }
+    })
 
     // POSTs name, movie list, and sharecode(if any)
     $("#share").click(function (){
