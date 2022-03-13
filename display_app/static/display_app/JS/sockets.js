34164e4ac117a1725776ff3a5a19a712f1d4b011
JamMor <JamMor@gmail.com>
2022-03-12 20:11:47 -0800

    Moved CSRF token getter to base.js

--- display_app/static/display_app/JS/sockets.js
+++ display_app/static/display_app/JS/sockets.js
@@ -1,35 +1,5 @@
 $(document).ready(function() {
 
-    //Prepare csrf token to be used outside of template.
-    function getCookie(name) {
-        let cookieValue = null;
-        if (document.cookie && document.cookie !== '') {
-            const cookies = document.cookie.split(';');
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
-                // Does this cookie string begin with the name we want?
-                if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
-                }
-            }
-        }
-        return cookieValue;
-    }
-
-    function csrfSafeMethod(method) {
-        // these HTTP methods do not require CSRF protection
-        return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-    }
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-
-    const csrftoken = getCookie('csrftoken');
     const api_key = "f4f5f258379baf10796e1d3aeb5add05";
     const image_link = "https://image.tmdb.org/t/p/";
 
