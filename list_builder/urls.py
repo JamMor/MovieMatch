11ce046919dc695831c0e93d69e06d2162322ede
JamMor <JamMor@gmail.com>
2022-04-16 12:53:11 -0700

    Add 'default_redirect' name for list_builder

--- list_builder/urls.py
+++ list_builder/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = "list_builder"
 urlpatterns = [
-    path('', views.index, name="list_builder")
+    path('', views.index, name="list_builder"),
+    path('', views.index, name="default_redirect")
 ]
