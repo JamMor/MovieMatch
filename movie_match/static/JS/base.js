d7cfa8dab0c92f73d37d615338cc699ae580bf8e
JamMor <JamMor@gmail.com>
2022-04-18 20:22:11 -0700

    Initialize dropdown and collapsible login forms

--- movie_match/static/JS/base.js
+++ movie_match/static/JS/base.js
@@ -32,6 +32,13 @@ $(document).ready(function() {
 
     //Initialize Mobile Nav Menu item.
     $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown({
+        alignment: 'right',
+        constrainWidth: false,
+        coverTrigger: false,
+        closeOnClick: false
+    });
+    $('.collapsible').collapsible();
 
     //CONSTRUCTOR ELEMENTS
     //==========================================================================
