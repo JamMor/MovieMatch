448e1e5d276d202175281678491b69441066ce01
JamMor <JamMor@gmail.com>
2022-05-04 01:27:31 -0700

    Initial Save list function

--- list_builder/urls.py
+++ list_builder/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "list_builder"
 urlpatterns = [
     path('', views.index, name="list_builder"),
+    path('save', views.save, name="save_list"),
     path('', views.index, name="default_redirect")
 ]
--- list_builder/views.py
+++ list_builder/views.py
@@ -3,7 +3,7 @@ from django.db import IntegrityError
 from django.shortcuts import redirect, render
 from django.http import JsonResponse
 from django.urls import reverse
-from list_builder.models import UserUUID
+from list_builder.models import UserUUID, SavedMovieList
 # from app_login_and_reg.models import User
 import json
 from channels.layers import get_channel_layer
@@ -16,8 +16,20 @@ def index(request):
     return render(request, 'list_builder/index.html')
 
 def save(request):
-    status = "failure"
+    response = {"status": "failure"}
 
-    response = {"status": status}
-    # response.update({"errors" : login_form.errors})
+    if not request.user.is_authenticated:
+        response.update({"errors" : "Only logged in users can be saved."})
+    else:
+        data = json.loads(request.body)
+        try:
+            print("Would save list:")
+            print([movie["title"] for movie in data["movie_list"]])
+            saved_list = SavedMovieList.objects.create(list_name="Test List")
+            # saved_list = SavedMovieList.objects.create_from_movie_list(list_of_movies = data['movie_list'], creator = user_uuid, list_name = data['list_name'])
+            response.update({"status" : "success"})
+        except Exception as err:
+            print(err)
+            response.update({"errors" : repr(err)})
+
     return JsonResponse(response)
