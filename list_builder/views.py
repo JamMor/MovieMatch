527cde3361628c1cf9e91cb56857a6b48c747734
JamMor <JamMor@gmail.com>
2022-05-06 03:06:41 -0700

    Save list function in server

--- list_builder/views.py
+++ list_builder/views.py
@@ -20,16 +20,22 @@ def save(request):
 
     if not request.user.is_authenticated:
         response.update({"errors" : "Only logged in users can be saved."})
+
     else:
         data = json.loads(request.body)
-        try:
-            print("Would save list:")
-            print([movie["title"] for movie in data["movie_list"]])
-            saved_list = SavedMovieList.objects.create(list_name="Test List")
-            # saved_list = SavedMovieList.objects.create_from_movie_list(list_of_movies = data['movie_list'], creator = user_uuid, list_name = data['list_name'])
-            response.update({"status" : "success"})
-        except Exception as err:
-            print(err)
-            response.update({"errors" : repr(err)})
+        list_name = data.get("list_name")
+        movie_list = data.get("movie_list")
+        if not movie_list:
+            response.update({"errors" : "Cannot save empty list."})
+        else:
+            try:
+                user_uuid = get_or_set_uuid(request)
+                print("Would save list:", list_name)
+                print([movie["title"] for movie in movie_list])
+                saved_list = SavedMovieList.objects.create_from_movie_list(list_of_movies=movie_list, creator=user_uuid, list_name=list_name)
+                response.update({"status" : "success"})
+            except Exception as err:
+                print(err)
+                response.update({"errors" : repr(err)})
 
     return JsonResponse(response)
