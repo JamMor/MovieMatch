6d538af6fc6916064f63442d25e9c110b2f2c1f3
JamMor <JamMor@gmail.com>
2022-05-10 12:40:29 -0700

    Working local db Movie add function
    Takes list of movie id's and calls movieDB api for info on any that
    don't exist in local DB. Adds their info to local DB and return

--- list_builder/moviedb_api_caller.py
+++ list_builder/moviedb_api_caller.py
@@ -1,28 +1,56 @@
 from list_builder.models import Movie
 import requests
 
-def movie_updater(movie_id_list):
+def update_from_movie_id_list(movie_id_list):
+    """
+    Takes a list of movieDB ID's as integers, and checks which are in local DB.
+    Ones that aren't in local database have info retrieved by API call to movieDB,
+    and added to local DB.
+
+    Returns tuple: Whether all movies were saved, id's saved, id's not saved.
+    """
     api_key = "f4f5f258379baf10796e1d3aeb5add05"
 
+    #Determines which movies are not in local DB
     movie_id_set = set(movie_id_list)
     movies_in_db = set(Movie.objects.filter(
         movie_id__in=movie_id_set).values_list("movie_id", flat=True))
     movies_not_in_db = movie_id_set - movies_in_db
     
-    print("In DB: ", movies_in_db)
-    print("Not in DB: ", movies_not_in_db)
+    new_movie_list = []
 
+    #Retrieves info for each movie not in local DB
     for movie_id in movies_not_in_db:
-        print(f'FOR: {movie_id}')
-        print(f'URL = https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US')
         response = requests.get(
             f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US')
-        print(response.status_code)
         
         if response.status_code == requests.codes.ok:
-            print(response.json().get('title'))
+            data = response.json()
+
+            new_movie_list.append(
+                Movie(movie_id = data.get("id"),
+                      title = data.get("title"),
+                      overview = data.get("overview"),
+                      poster_path = data.get("poster_path"),
+                      release_date = data.get("release_date")
+                      )
+            )
+
         else:
+            print(f'Could not retrieve data for: {movie_id}')
             print(response.json())
 
-    print("Done querying movies.")
-    return
+    #Saves movies with retrieved data to DB
+    try:
+        Movie.objects.bulk_create(new_movie_list)
+        print("Succesfully added to database: ")
+        print([movie.title for movie in new_movie_list])
+        movies_in_db.update([movie.movie_id for movie in new_movie_list])
+    except Exception as err:
+        print("Movie bulk creation failed.")
+        print(err)
+    
+    if movie_id_set == movies_in_db:
+        return True, movies_in_db, []
+    else:
+        return False, movies_in_db, movie_id_set - movies_in_db
