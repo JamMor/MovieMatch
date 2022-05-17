5816f8297dd450d28af9e544947daf95fb56f089
JamMor <JamMor@gmail.com>
2022-05-17 02:53:21 -0700

    Rename variables for clarity. No functional change

--- elimination_room/static/elimination_room/JS/match_builder.js
+++ elimination_room/static/elimination_room/JS/match_builder.js
@@ -1,20 +1,19 @@
 const share_list_prefix ="shared";
     
 //Adds new movies into DOM when a shared list is updated.
-function movieListBuilder(old_list, updated_list){
+function movieListBuilder(current_list, new_list){
     let added_movie_list;
-    if (old_list.length > 0){
-        // Get movies from updated list that aren't already in list to be added
-        added_movie_list = updated_list
-            .filter(new_movie => !old_list
-                .some(old_movie => (old_movie.movie_id == new_movie.movie_id)))
+    if (current_list.length > 0){
+        // Filter movies from updated list that aren't already in old list
+        //FLAG maybe just compare sets of ID's
+        added_movie_list = new_list
+            .filter(new_movie => !current_list
+                .some(current_movie => (current_movie.movie_id == new_movie.movie_id)))
     }
     //If no movies in list yet, all are to be added
-    else {added_movie_list = updated_list}
+    else {added_movie_list = new_list}
     
     added_movie_list.forEach((movie) => {
-        //Rename attributes for MovieCard
-        let {shared_movie_id:id, ...rest} = movie
         $("#movie_list")
             .append(
                 construct.MovieCard(share_list_prefix, movie.movie_id, movie, ["remove", "info"])
@@ -29,9 +28,9 @@ function movieListBuilder(old_list, updated_list){
         return this.id.split("_")[1];
         })
         .get()
-    let new_movie_ids = new Set(updated_list.map(x => x.shared_movie_id))
+    let new_movie_ids = new Set(new_list.map(x => x.shared_movie_id))
     let list_verified = false;
-    if(dom_movie_ids.length == updated_list.length){
+    if(dom_movie_ids.length == new_list.length){
         for(let id of dom_movie_ids){
             if(!new_movie_ids.has(id)){
                 break;
