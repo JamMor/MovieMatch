579a8b37812ee893e43abbf8b1252e6486c76933
JamMor <JamMor@gmail.com>
2022-06-01 16:42:36 -0700

    Split share_modal javascript into own file

--- list_builder/static/list_builder/JS/index.js
+++ list_builder/static/list_builder/JS/index.js
@@ -138,35 +138,6 @@ $(document).ready(function() {
         $("div.carousel").animate({height: "0px"}, 150).html("");
     });
 
-    // POSTs name, movie list, and sharecode(if any)
-    $("#share-btn").click(function (){
-        let sharecode = $("#sharecode").val().toUpperCase();
-        if (!/^$|^[23456789ABCDEFGHJKLMNPQRSTUVWXYZ]{8}$/.test(sharecode)){
-            console.log("Invalid Sharecode format.")
-            return
-        }
-
-        let nickname = $("#nickname").val();
-        console.log("Submitting!")
-
-        let tmdb_ids = movie_list.map(movie => movie.tmdb_id)
-        // console.log("DATA for Django: ", {"sharecode": sharecode, "nickname": nickname, "movie_list": movie_list});
-        $.post("match/", JSONSizeData(JSON.stringify({"sharecode": sharecode, "nickname": nickname, "tmdb_ids": tmdb_ids})),"json")
-            .done(function (data) {
-                console.log(data);
-                
-                if (data['status'] == "success") { 
-                    window.location.href = `/match/${data.sharecode}`; 
-                }
-                else { 
-                    console.log(data['status']) 
-                }
-            })
-            .fail(function() {
-                console.log( "ERROR: Failed to send movie list." );
-                })
-    })
-
     // Button to clear current user list
     $("#clear").click(function (){
         movie_list = [];
