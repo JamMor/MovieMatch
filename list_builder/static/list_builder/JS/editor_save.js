a98df0688d68d5a00e679a74e8dc6c6bb9631e2f
JamMor <JamMor@gmail.com>
2022-06-14 20:34:01 -0700

    editor_save handles click of save confirmation

---
+++ list_builder/static/list_builder/JS/editor_save.js
@@ -0,0 +1,12 @@
+$("#save-list-confirm").click(function (e){
+    e.preventDefault();
+    
+    if(movie_list.length == 0){
+        listOperation.saveStatusToast(list_name, "empty")
+        return
+    }
+
+    let tmdb_ids = movie_list.map(movie => movie.tmdb_id)
+
+    listOperation.saveList({"tmdb_ids" : tmdb_ids, "list_name" : savedListName, "list_id": savedListId})
+})
