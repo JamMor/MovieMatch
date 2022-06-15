8b97a648aa6ea7bdd3f177339362fba327085fbf
JamMor <JamMor@gmail.com>
2022-06-14 20:25:52 -0700

    Added save url to accept no parameter
    Template would not render url without parameter

--- list_builder/urls.py
+++ list_builder/urls.py
@@ -5,6 +5,7 @@ app_name = "list_builder"
 urlpatterns = [
     path('', views.index, name="list_builder"),
     path('list_manager', views.list_manager, name="list_manager"),
+    path('save/<int:list_id>', views.save, name="save_list"),
     path('save', views.save, name="save_list"),
     path('edit/<int:list_id>', views.edit, name="edit_list"),
     path('delete/<int:list_id>', views.delete, name="delete_list"),
