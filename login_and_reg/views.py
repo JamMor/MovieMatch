1c11419eaeebc0baef79bc5e78cc2b4a00d26ffc
JamMor <JamMor@gmail.com>
2022-06-30 23:51:39 -0700

    Add FLAG to comment to find later

--- login_and_reg/views.py
+++ login_and_reg/views.py
@@ -10,7 +10,7 @@ from .forms import RegistrationForm, PersonaForm
 from list_builder.models import Persona
 from list_builder.persona_assigner import get_or_set_persona
 
-#Make class based
+#FLAG Make class based
 def register_view(request):
     if request.method == 'POST':
         user_form = RegistrationForm(request.POST, prefix='user')
