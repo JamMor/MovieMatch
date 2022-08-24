d28d28d68b7a2c7d2fc3f7586199819eaa9165fa
JamMor <JamMor@gmail.com>
2022-08-23 22:32:53 -0700

    Add static_root to base settings

--- movie_match/settings/base.py
+++ movie_match/settings/base.py
@@ -135,6 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "movie_match/static", 
 ]
+STATIC_ROOT =  BASE_DIR / 'staticfiles/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
