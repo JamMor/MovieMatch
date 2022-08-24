1dc62654ee2e0405c23adb7de3357242f6043592
JamMor <JamMor@gmail.com>
2022-08-23 22:37:08 -0700

    Print diagnostics for testing settings variables

--- movie_match/settings/dev.py
+++ movie_match/settings/dev.py
@@ -1,5 +1,10 @@
 from .base import *
+from .diag import *
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+environ_status(f'{__name__} Environ')
+settings_status(f'Settings: {__name__}')
+dotenv_status(dotenv_values())
