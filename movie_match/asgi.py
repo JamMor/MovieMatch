86228128783358835381341a6a224186e0874194
JamMor <JamMor@gmail.com>
2022-08-23 22:40:07 -0700

    Change default settings to new base settings

--- movie_match/asgi.py
+++ movie_match/asgi.py
@@ -14,7 +14,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_match.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_match.settings.base')
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
--- movie_match/wsgi.py
+++ movie_match/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_match.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movie_match.settings.base')
 
 application = get_wsgi_application()
