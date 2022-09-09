574c19a5a5e105d80c58acb25da1aaa2f7991ae9
JamMor <JamMor@gmail.com>
2022-09-08 18:07:13 -0700

    Disable environment diagnostics

--- movie_match/settings/dev.py
+++ movie_match/settings/dev.py
@@ -5,6 +5,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-environ_status(f'{__name__} Environ')
-settings_status(f'Settings: {__name__}')
-dotenv_status(dotenv_values())
+# environ_status(f'{__name__} Environ')
+# settings_status(f'Settings: {__name__}')
+# dotenv_status(dotenv_values())
--- movie_match/settings/prod.py
+++ movie_match/settings/prod.py
@@ -6,6 +6,6 @@ DEBUG = False
 env_hosts = os.environ.get("ALLOWED_HOSTS", "").split(",")
 ALLOWED_HOSTS = [] if not any(env_hosts) else env_hosts
 
-environ_status(f'{__name__} Environ')
-settings_status(f'Settings: {__name__}')
-dotenv_status(dotenv_values())
+# environ_status(f'{__name__} Environ')
+# settings_status(f'Settings: {__name__}')
+# dotenv_status(dotenv_values())
