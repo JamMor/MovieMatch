88bc8c72ba7fc92c56b62f1f0e8e154e2bb2ec75
JamMor <JamMor@gmail.com>
2022-09-20 01:33:45 -0700

    Read CSRF_COOKIE_DOMAIN setting from env
    https://docs.djangoproject.com/en/4.1/ref/csrf/#how-it-works
    Post requests getting 403 response. Explicitly allow moviematch
    subdomain to POST.

--- movie_match/settings/prod.py
+++ movie_match/settings/prod.py
@@ -6,6 +6,9 @@ DEBUG = False
 env_hosts = os.environ.get("ALLOWED_HOSTS", "").split(",")
 ALLOWED_HOSTS = [] if not any(env_hosts) else env_hosts
 
+env_cookie_domains = os.environ.get("CSRF_COOKIE_DOMAIN", "").split(",")
+CSRF_COOKIE_DOMAIN = [] if not any(env_cookie_domains) else env_cookie_domains
+
 # environ_status(f'{__name__} Environ')
 # settings_status(f'Settings: {__name__}')
 # dotenv_status(dotenv_values())
