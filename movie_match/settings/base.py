3bee0f46e8d8657266c876d1497bb9c69804abf5
JamMor <JamMor@gmail.com>
2022-09-19 21:42:33 -0700

    Added logging to stream (console)

--- movie_match/settings/base.py
+++ movie_match/settings/base.py
@@ -128,6 +128,21 @@ PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
