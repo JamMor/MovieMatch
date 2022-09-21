a9fd79d119728e9f3326c2cc6dea656cd3393557
JamMor <JamMor@gmail.com>
2022-09-20 19:19:48 -0700

    Secure Session and csrf cookies

--- movie_match/settings/prod.py
+++ movie_match/settings/prod.py
@@ -6,7 +6,10 @@ DEBUG = False
 env_hosts = os.environ.get("ALLOWED_HOSTS", "").split(",")
 ALLOWED_HOSTS = [] if not any(env_hosts) else env_hosts
 
+# HTTPs settings
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 env_cookie_domains = os.environ.get("CSRF_COOKIE_DOMAIN", "").split(",")
 CSRF_COOKIE_DOMAIN = [] if not any(env_cookie_domains) else env_cookie_domains
