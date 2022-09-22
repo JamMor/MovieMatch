0c2febe5d21e109bab508d2f553b6cdeb582e79a
JamMor <JamMor@gmail.com>
2022-09-21 19:42:58 -0700

    CSRF_COOKIE_DOMAIN should be string not list

--- movie_match/settings/prod.py
+++ movie_match/settings/prod.py
@@ -11,8 +11,8 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
-env_cookie_domains = os.environ.get("CSRF_COOKIE_DOMAIN", "").split(",")
-CSRF_COOKIE_DOMAIN = [] if not any(env_cookie_domains) else env_cookie_domains
+env_cookie_domains = os.environ.get("CSRF_COOKIE_DOMAIN", None)
+CSRF_COOKIE_DOMAIN = env_cookie_domains
 
 # environ_status(f'{__name__} Environ')
 # settings_status(f'Settings: {__name__}')
