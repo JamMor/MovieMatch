e701dfad26b5851df4ef25b9633c0b2ee1e0faf6
JamMor <JamMor@gmail.com>
2022-12-20 16:33:08 -0800

    Working Account Deletion AJAX JS handler

--- login_and_reg/static/login_and_reg/js/account_delete_modal.js
+++ login_and_reg/static/login_and_reg/js/account_delete_modal.js
@@ -9,6 +9,33 @@ $(document).ready(function() {
     $("#account-delete-confirm").on("click", function(e){
         e.preventDefault();
         console.log("Delete request sent.")
+        let deleteForm = document.querySelector("#account-delete-form");
+        const deleteFormData = new FormData(deleteForm);
+        $.ajax({
+            url: `settings/delete-account`,
+            method:"POST",
+            data: deleteFormData,
+            // processData and contentType needed to properly send formData
+            // jQuery tries to make it a string
+            processData: false,
+            contentType: false
+        })
+        .done(function(data) {
+            console.log(data);
+            if(data['status'] == "success"){
+                $('#delete-modal').modal('close');
+                console.log("Delete Success.")
+                window.location.replace(`/`);
+            }
+            else {
+                console.log("Failed to delete.")
+            }
 
+        })
+        .fail(function() {
+            console.log( "Failed to send delete request." );
+        })
+        
     })
 
+})
