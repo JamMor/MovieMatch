2adbe7ffcd07dbe08e7a4dd6021346a983869bf9
JamMor <JamMor@gmail.com>
2023-01-30 14:21:09 -0800

    Init modularize consumer commands. Elimination.
    Turned into function and copied imports.
    Will need to change return values and simplify logic

---
+++ elimination_room/command_requests.py
@@ -0,0 +1,93 @@
+import json
+from random import randint
+import random
+from datetime import timedelta
+from django.utils import timezone
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import JsonWebsocketConsumer
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+
+from list_builder.models import Persona
+from elimination_room.models import SharedMovie, ShareRoomUser, SharedMovieList
+from .serializer import SharedListEncoder
+from .consumer_utils import find_next_index
+from .command_requests import request_eliminat
+
+def request_eliminate(self, content):
+    active_share_users_qs = ShareRoomUser.objects.filter(list__sharecode = self.sharecode, is_active = True).order_by('created_at')
+    #If it isn't any user's turn, elimination hasn't started. Return failed msg
+    if active_share_users_qs.filter(is_users_turn = True).count() == 0:
+        self.send_json({
+                'type': 'eliminate_message',
+                'status' : 'failed',
+                'error_message' : "List not set to allow elimination."
+        })
+        return
+    #If it isn't THIS user's turn. Return failed msg
+    this_user = active_share_users_qs.get(persona__uuid = self.persona_uuid)
+    if not this_user.is_users_turn:
+        self.send_json({
+                'type': 'eliminate_message',
+                'status' : 'failed',
+                'error_message' : "Not this users turn."
+        })
+        return
+    #If elimination has started:
+    shared_movie_id = content['shared_movie_id']
+    uneliminated_movies_qs = SharedMovie.objects.filter(shared_list__sharecode = self.sharecode, is_eliminated = False)
+    movies_left = uneliminated_movies_qs.count()
+
+    #If available movies > 1
+    if movies_left > 1:
+        #Eliminate movie
+        try:
+            shared_movie = uneliminated_movies_qs.get(id=shared_movie_id)
+        except SharedMovie.DoesNotExist:
+            print("Can't find selected movie (probably already eliminated).")
+            self.send_json({
+                'type': 'eliminate_message',
+                'status' : 'failed',
+                'error_message' : 'Shared movie not found in uneliminated movies.'
+            })
+            return
+        shared_movie.is_eliminated = True
+        shared_movie.save()
+        movies_left -= 1
+        
+        #Pick next user
+        current_user = active_share_users_qs.get(persona__uuid = self.persona_uuid)
+        current_user.is_users_turn = False
+        current_user.save()
+        next_index = find_next_index(self.persona_uuid, list(active_share_users_qs.values_list('persona__uuid', flat=True)))
+        if next_index == None:
+            print("No available user for turn.")
+            return
+        next_user = active_share_users_qs[next_index]
+        next_user.is_users_turn = True
+        next_user.save()
+
+        #Confirm Removal for Group
+        async_to_sync(self.channel_layer.group_send)(
+            self.match_group_name,
+            {
+                'type': 'eliminate_message',
+                'shared_movie_id' : shared_movie_id,
+                'eliminating_uuid' : self.persona_uuid,
+                'next_eliminating_uuid' : next_user.persona.uuid
+            }
+        )
+
+    #If last possible elimination
+    if movies_left == 1:
+        final_movie = SharedMovie.objects.filter(shared_list__sharecode = self.sharecode, is_eliminated = False).first()
+        #Send final movie signal
+        async_to_sync(self.channel_layer.group_send)(
+                self.match_group_name,
+                {
+                    'type': 'final_message',
+                    'shared_movie_id' : final_movie.id
+                }
+            )
+        #Set all to no one's turn
+        active_share_users_qs.filter(is_users_turn = True).update(is_users_turn = False)
