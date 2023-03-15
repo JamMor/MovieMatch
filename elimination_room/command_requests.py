183b3b2f0921ad438199ee7d8768868616f8408c
JamMor <JamMor@gmail.com>
2023-03-15 14:26:24 -0700

    Convert modular functions to use CommandResponses

--- elimination_room/command_requests.py
+++ elimination_room/command_requests.py
@@ -12,30 +12,31 @@ from list_builder.models import Persona
 from elimination_room.models import SharedMovie, ShareRoomUser, SharedMovieList
 from .serializer import SharedListEncoder as SharedListJsonEncoder
 from .consumer_utils import find_next_index
-from .json_response import SuccessJsonClassObject, FailedJsonClassObject
+from .json_response import SuccessJsonClassObject, FailedJsonClassObject, SuccessfulCommandResponse, FailedCommandResponse
 
 def request_eliminate(sharecode, persona_uuid, content):
     """
     Request to eliminate a movie from the list.
     Returns object with 
     """
+    command = "eliminated"
 
     active_share_users_qs = ShareRoomUser.objects.filter(list__sharecode = sharecode, is_active = True).order_by('created_at')
 
     #If it isn't any user's turn, elimination hasn't started. Return failed msg
     if active_share_users_qs.filter(is_users_turn = True).count() == 0:
-        response_object = FailedJsonClassObject(errors=["List not set to allow elimination."])
-        return response_object
+        return FailedCommandResponse(command=command, errors=["List not set to allow elimination."])
     
     #If it isn't THIS user's turn. Return failed msg
     this_user = active_share_users_qs.get(persona__uuid = persona_uuid)
     if not this_user.is_users_turn:
-        response_object = FailedJsonClassObject(errors=["Not this users turn."])
-        return response_object
+        return FailedCommandResponse(command=command, errors=["Not this users turn."])
     #If elimination has started:
     shared_movie_id = content['shared_movie_id']
     uneliminated_movies_qs = SharedMovie.objects.filter(shared_list__sharecode = sharecode, is_eliminated = False)
     movies_left = uneliminated_movies_qs.count()
+
+    successful_response = SuccessfulCommandResponse(command=command)
 
     #If available movies > 1
     if movies_left > 1:
@@ -44,8 +45,7 @@ def request_eliminate(sharecode, persona_uuid, content):
             shared_movie = uneliminated_movies_qs.get(id=shared_movie_id)
         except SharedMovie.DoesNotExist:
             print("Can't find selected movie (probably already eliminated).")
-            response_object = FailedJsonClassObject(errors=["Shared movie not found in uneliminated movies."])
-            return response_object
+            return FailedCommandResponse(command=command, errors=["Shared movie not found in uneliminated movies."])
         shared_movie.is_eliminated = True
         shared_movie.save()
         movies_left -= 1
@@ -63,56 +63,58 @@ def request_eliminate(sharecode, persona_uuid, content):
         next_user.is_users_turn = True
         next_user.save()
 
-        #Confirm Removal for Group
-        response_object = SuccessJsonClassObject(data={
+        successful_response.add_data({
             "shared_movie_id": shared_movie_id,
             "eliminating_uuid": persona_uuid,
             "next_eliminating_uuid": next_user.persona.uuid
         })
-        return response_object
 
     #If last possible elimination
     if movies_left == 1:
         final_movie = SharedMovie.objects.filter(shared_list__sharecode = sharecode, is_eliminated = False).first()
-        response_object = SuccessJsonClassObject(data={
-            "final_shared_movie_id": final_movie.id,
-        })
+
+        successful_response.add_data({"final_shared_movie_id": final_movie.id})
 
         #Set all to no one's turn
         active_share_users_qs.filter(is_users_turn = True).update(is_users_turn = False)
         
-        return response_object
+    return successful_response
     
 def request_initialize(sharecode):
+    command = "initialized"
+
     try:
         model_dict = SharedListJsonEncoder(sharecode)
-        return SuccessJsonClassObject(data={"share_list": model_dict})
+        return SuccessfulCommandResponse(command=command, data={"share_list": model_dict})
     except:
-        return FailedJsonClassObject(errors=["Error initializing list."])
+        return FailedCommandResponse(command=command, errors=["Error initializing list."])
     
 def request_elimination_start(sharecode):
+    command = "elimination_started"
     active_share_users_qs = ShareRoomUser.objects.filter(list__sharecode = sharecode, is_active = True)
     
     users_eliminating = active_share_users_qs.filter(is_users_turn = True).count()
     if users_eliminating > 0:
-        return FailedJsonClassObject(errors=["Elimination already in progress."])
+        return FailedCommandResponse(command=command, errors=["Elimination already in progress."])
 
     if SharedMovie.objects.filter(shared_list__sharecode = sharecode).count() < 2:
-        return FailedJsonClassObject(errors=["Must be at least 2 movies in list to begin eliminating."])
+        return FailedCommandResponse(command=command, errors=["Must be at least 2 movies in list to begin eliminating."])
 
     #Randomly pick user to start
     eliminating_user = random.choice(active_share_users_qs)
     eliminating_user.is_users_turn = True
     eliminating_user.save()
                     
-    return SuccessJsonClassObject(data={"eliminating_uuid": eliminating_user.persona.uuid})
+    return SuccessfulCommandResponse(command=command, data={"eliminating_uuid": eliminating_user.persona.uuid})
 
 def request_refresh_list(sharecode):
+    command = "refreshed"
+
     SharedMovie.objects.filter(shared_list__sharecode = sharecode).update(is_eliminated = False)
 
     try:
         model_dict = SharedListJsonEncoder(sharecode)
-        return SuccessJsonClassObject(data={"share_list": model_dict})
+        return SuccessfulCommandResponse(command=command, data={"share_list": model_dict})
     except:
-        return FailedJsonClassObject(errors=["Error refreshing list."])
+        return FailedCommandResponse(command=command, errors=["Error refreshing list."])
     
