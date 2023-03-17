5cddf9444d68b3c912aa74a9d03f9ada43e433eb
JamMor <JamMor@gmail.com>
2023-03-17 13:29:15 -0700

    Temporary handling of new final movie response
    final id now included in last elimination response

--- elimination_room/static/elimination_room/js/sockets.js
+++ elimination_room/static/elimination_room/js/sockets.js
@@ -77,6 +77,31 @@ $(document).ready(function() {
                 M.toast({html: toastHtml})
                 
                 setUserTurn(next_uuid);
+
+                // ====================FLAG====Temporary Final Fix=========================================
+                if(commandData.hasOwnProperty('final_shared_movie_id')) {
+                    let finalSharedId = commandData.final_shared_movie_id;
+                    let finalMovie = movie_list.find(movie => movie.shared_movie_id == finalSharedId);
+                    console.log(`${finalMovie.title} is the final choice!`)
+                    
+                    getMoreMovieInfo(finalMovie.tmdb_id)
+                        .done(returnInfo => {
+                            console.log(returnInfo)
+                            let finalMovieInfo = returnInfo ?? finalMovie
+                            openMoreInfoModal(finalMovieInfo, "final_modal")
+                        })
+                        .fail(function(){
+                            console.log("AJAX error")
+                        })
+
+
+                    let {styleClass, icons, statusText} = getStatusBarProperties("final");
+                    $('#status-btn').removeClass().addClass(styleClass);
+                    $('#status-btn i').html(icons);
+                    $('#status-btn span').html(statusText);
+                }
+                // ==========================================================================================
+
             }
             
             //User connected
