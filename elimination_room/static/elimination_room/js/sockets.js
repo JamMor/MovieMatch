91c7bb550e487ef3589b09b87edd6933e0b2f11e
JamMor <JamMor@gmail.com>
2023-03-30 11:16:36 -0700

    Fixed error. Instantiate variable locally

--- elimination_room/static/elimination_room/js/sockets.js
+++ elimination_room/static/elimination_room/js/sockets.js
@@ -49,6 +49,7 @@ $(document).ready(function() {
                 return
             }
             //Successful command
+            let commandData;
             if (responseData.hasOwnProperty('data')){
                 commandData = responseData.data
             }
