43897a949342d707b3ee1903842eaf4846763b4f
JamMor <JamMor@gmail.com>
2023-04-22 18:04:13 -0700

    Init commit to a separate elimination order file

---
+++ elimination_room/user_management.py
@@ -0,0 +1,11 @@
+#Add to end of queue
+def add_user_to_end_of_queue(end_user, share_list, current_round):
+    pass
+
+# Assign next round order, return first eliminating user
+def assign_round_order(sharecode):
+    pass
+
+# Returns next user in queue, or None if no more users
+def select_next_eliminating_user(sharecode, current_round, turn):
+    pass
