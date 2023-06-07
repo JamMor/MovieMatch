700749c0702342758c7034bf1835c7906da158bb
JamMor <JamMor@gmail.com>
2023-06-07 03:25:22 -0700

    Added type hinting to json classes

--- elimination_room/json_response.py
+++ elimination_room/json_response.py
@@ -1,6 +1,6 @@
 #This is a base class for a json response
 class JsonClassObject:
-    def __init__(self, message = None):
+    def __init__(self, message:str = None):
         if message == None:
             message = ""
         self.message = message
@@ -11,7 +11,7 @@ class JsonClassObject:
 
 #This is a child class for a successful json response
 class SuccessJsonClassObject(JsonClassObject):
-    def __init__(self, message = None, data = None):
+    def __init__(self, message:str = None, data:dict[str,str] = None):
         super().__init__(message)
         self.status = "success"
         if data == None:
@@ -24,7 +24,7 @@ class SuccessJsonClassObject(JsonClassObject):
 
 #This is a child class for an error json response
 class FailedJsonClassObject(JsonClassObject):
-    def __init__(self, message = None, errors = None):
+    def __init__(self, message:str = None, errors:list[str] = None):
         super().__init__(message)
         self.status = "failure"
         if errors == None:
@@ -37,19 +37,19 @@ class FailedJsonClassObject(JsonClassObject):
 
 #This is a base class for an elimination room socket command type
 class CommandType():
-    def __init__(self, command = None):
+    def __init__(self, command:str = None):
         if command == None:
             command = ""
         self.command = command
 
 #This is a child class for a successful json response to an elimination room socket command
 class SuccessfulCommandResponse(SuccessJsonClassObject, CommandType):
-    def __init__(self, message = None, data = None, command = None):
+    def __init__(self, message:str = None, data:dict[str,str] = None, command:str = None):
         super().__init__(message, data)
         CommandType.__init__(self, command)
 
 #This is a child class for a failed json response to an elimination room socket command
 class FailedCommandResponse(FailedJsonClassObject, CommandType):
-    def __init__(self, message = None, errors = None, command = None):
+    def __init__(self, message:str = None, errors:list[str] = None, command:str = None):
         super().__init__(message, errors)
         CommandType.__init__(self, command)
