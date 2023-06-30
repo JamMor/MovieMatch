143fc87189dcdfe2782edcc49c79ea33cfe94878
JamMor <JamMor@gmail.com>
2023-06-30 16:34:26 -0700

    Add reorder DOM users function to userListBuilder

--- elimination_room/static/elimination_room/js/user_builder.js
+++ elimination_room/static/elimination_room/js/user_builder.js
@@ -33,6 +33,27 @@ function removeUserFromDom(uuid){
     $(`#user_${uuid}`).remove()
 }
 
+function reorderUsersInDom(uuids, user_list){
+    
+    //
+
+    // Get current user elements
+    let userContainer = $('#user_list')
+    let userElements = userContainer.children()
+
+    // Get sorted list of uuids
+    let sortedUUIDs = uuids.sort((a, b) => user_list[a].position - user_list[b].position)
+
+    // Insertion sort user elements and update data-position
+    userElements.each(function(elementIndex){
+        let elementUUID = $(this).attr('id').split("_")[1]
+        $(this).data('position', user_list[elementUUID].position)
+        let newIndex = sortedUUIDs.indexOf(elementUUID)
+        if (newIndex != elementIndex){
+            $(this).insertBefore(userElements.eq(newIndex))
+        }
+    })
+}
 
 //Updates user list and renders in DOM when intialized or share list updated.
 function userListBuilder(old_list, updated_list){
@@ -40,6 +61,7 @@ function userListBuilder(old_list, updated_list){
     let added_uuid_list = new Set();
     let deleted_uuid_list = new Set();
     let updated_uuids = new Set(Object.keys(updated_list));
+    let existing_uuid_list = new Set();
 
     if (old_uuids.size > 0){
         added_uuid_list = new Set(updated_uuids)
@@ -49,6 +71,7 @@ function userListBuilder(old_list, updated_list){
         for (let old_uuid of old_uuids){
             if (added_uuid_list.has(old_uuid)){
                 added_uuid_list.delete(old_uuid)
+                existing_uuid_list.add(old_uuid)
             }
             else {
                 deleted_uuid_list.add(old_uuid)
@@ -59,9 +82,20 @@ function userListBuilder(old_list, updated_list){
         added_uuid_list = updated_uuids
     }
 
+    // Remove deleted users from DOM
     for (let deleted_uuid of deleted_uuid_list){
         removeUserFromDom(deleted_uuid);
     }
+
+    // Run reorder function if any users have changed position
+    for (let this_uuid of existing_uuid_list){
+        if (old_list[this_uuid].position != updated_list[this_uuid].position){
+            reorderUsersInDom(existing_uuid_list, updated_list)
+            break
+        }
+    }
+
+    // Add new users to DOM
     for (let added_uuid of added_uuid_list){
         addUserToDom(added_uuid, updated_list[added_uuid]);
     }
