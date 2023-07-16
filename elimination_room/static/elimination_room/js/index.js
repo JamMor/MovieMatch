50f3612f1bb4539efca912caed4968687659e9d0
JamMor <JamMor@gmail.com>
2023-07-16 11:15:01 -0700

    Modularized Copy Sharecode

--- elimination_room/static/elimination_room/js/index.js
+++ elimination_room/static/elimination_room/js/index.js
@@ -1,7 +1,9 @@
 //Imports
 import * as ClickHandlers from "./click-handlers.js";
+import * as CopyToClipBoard from "./sharecode_copy.js";
 
 //Elimination Room Entry Point
 $(document).ready(function() {
     ClickHandlers.init();
+    CopyToClipBoard.init();
 });
