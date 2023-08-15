266d99d5c5610f7f5f542e9c208b298729e2464b
JamMor <JamMor@gmail.com>
2023-08-15 14:32:36 -0700

    Modularized TableCollapse JS

--- list_builder/static/list_builder/js/list_manager/index.js
+++ list_builder/static/list_builder/js/list_manager/index.js
@@ -1,8 +1,10 @@
 import * as DeleteListHandler from "../delete_modal.js"
 import * as SliderX from "../slider.js"
+import * as TableCollapse from "../table-collapse.js"
 
 //List Manager Entry Point
 $(document).ready(function() {
     SliderX.init();
+    TableCollapse.init();
     DeleteListHandler.init();
 });
