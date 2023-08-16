70822caaeabc56b843633ac0e887b613bf5b3d95
JamMor <JamMor@gmail.com>
2023-08-16 13:07:36 -0700

    Moved code outside of init() function

--- list_builder/static/list_builder/js/slider.js
+++ list_builder/static/list_builder/js/slider.js
@@ -1,44 +1,43 @@
+const sliders = document.querySelectorAll('.slider-x');
+
+let pos = { left: 0, x: 0 };
+let activeSlider;
+
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+
+    // Scroll the element
+    activeSlider.scrollLeft = pos.left - dx;
+};
+
+const mouseUpHandler = function () {
+    // activeSlider.style.cursor = 'grab';
+    activeSlider.style.removeProperty('user-select');
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+};
+
+const mouseDownHandler = function (e) {
+    e.preventDefault();
+    // this.style.cursor = 'grabbing';
+    this.style.userSelect = 'none';
+
+    pos = {
+        // Current scroll position
+        left: this.scrollLeft,
+        // Current mouse position
+        x: e.clientX,
+    };
+
+    activeSlider = this;
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+};
+
 const init = () => {
-    const sliders = document.querySelectorAll('.slider-x');
-
-    let pos = { left: 0, x: 0 };
-    let activeSlider;
-
-    const mouseMoveHandler = function (e) {
-        // How far the mouse has been moved
-        const dx = e.clientX - pos.x;
-
-        // Scroll the element
-        activeSlider.scrollLeft = pos.left - dx;
-    };
-
-    const mouseUpHandler = function () {
-        // activeSlider.style.cursor = 'grab';
-        activeSlider.style.removeProperty('user-select');
-
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler);
-    };
-
-    const mouseDownHandler = function (e) {
-        e.preventDefault();
-        // this.style.cursor = 'grabbing';
-        this.style.userSelect = 'none';
-
-        pos = {
-            // Current scroll position
-            left: this.scrollLeft,
-            // Current mouse position
-            x: e.clientX,
-        };
-
-        activeSlider = this;
-
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    };
-
-
     // Attach the handler
     sliders.forEach(slider => {
         slider.addEventListener('mousedown', mouseDownHandler);
