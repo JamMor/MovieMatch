83fb5e6688647e7064f467b69bde742554370d71
JamMor <JamMor@gmail.com>
2023-08-22 14:31:32 -0700

    Use modularized applyTooltip code

--- login_and_reg/static/login_and_reg/js/register.js
+++ login_and_reg/static/login_and_reg/js/register.js
@@ -1,9 +1,5 @@
+import {applyTooltips} from "/static/js/form_functions.js"
+
 $(document).ready(function() {
-    $('form script').each(function () {
-        let thisId = $(this).attr("id").slice(5)
-        let thisValue = JSON.parse($(this).text());
-        $(`#${thisId}`).siblings("i").tooltip({
-            html: `<span>${thisValue}</span>`
-        });
-    })
+    applyTooltips();
 })
