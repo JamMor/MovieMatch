dd5054cd7ae94db12e219853ae1579741923468d
JamMor <JamMor@gmail.com>
2023-08-23 18:12:11 -0700

    Separate Searchbar JS

--- list_builder/static/list_builder/js/index.js
+++ list_builder/static/list_builder/js/index.js
@@ -1,5 +1,7 @@
 import * as ListBuilder from "./list_builder.js";
+import * as SearchBar from "./search_bar.js";
 
 $(document).ready(function(){
     ListBuilder.init();
+    SearchBar.init();
 });
--- list_builder/static/list_builder/js/list_builder.js
+++ list_builder/static/list_builder/js/list_builder.js
@@ -1,66 +1,6 @@
-import { Movie } from "/static/js/constructors.js";
 import { MovieCard } from "/static/js/DOMelements.js";
 
-const api_key = "f4f5f258379baf10796e1d3aeb5add05";
-
-let search_results = [];
-const search_prefix ="query";
 const movie_list_prefix ="movie";
-
-function activateSearch(){
-    $("div.carousel").fadeIn(100);
-    $("#movie_list").fadeTo(500, 0.1)
-}
-function deactivateSearch(){
-    $("div.carousel").fadeOut(100);
-    $("#movie_list").fadeTo(500, 1);
-}
-
-//FLAG Simplify and modularize
-function searchMovies() {
-    var searchQuery = this.value;
-    if (searchQuery.length >= 2) {
-        console.log(searchQuery)
-        $.get(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchQuery}`,
-            function () {
-                console.log("AJAX sent to TMDB");
-                return
-            }, "json")
-            .done(function (data) {
-                console.log(data);
-                if (data.results.length == 0) {
-                    $("div.carousel")
-                        /* Gets the initial rendered height from DOM (scrollHeight) 
-                            and animates. Callback sets height to auto. */
-                        .animate({ height: $('div.carousel').get(0).scrollHeight }, 200, function () {
-                            $(this).height('auto');
-                        })
-                        .html('<h6 class="center-align grey-text text-lighten-2">No results</h6>')
-                    return
-                }
-                $("div.carousel")
-                    .animate({ height: "400px" }, 200)
-                    .promise().done(function () {
-                        $(this)
-                            .html(data.results.map(({ id: tmdb_id, ...rest }) => {
-                                //Renames the movie DB ID to tmdb_id for Movie object creation
-                                let movieObj = new Movie({ tmdb_id, ...rest });
-                                search_results.push(movieObj);
-                                return MovieCard(search_prefix, tmdb_id, movieObj, ["add", "info"], "carousel-item")
-                            }).join('')
-                            )
-                            .carousel({
-                                dist: -50,
-                                numVisible: 10,
-                                noWrap: true
-                            })
-                    })
-            })
-    }
-    else if (searchQuery.length == 0) {
-        $("div.carousel").animate({ height: "0px" }, 150).html("")
-    }
-}
 
 function addMovieToDOM(thisMovieTmdbId){
     let thisMovie;
@@ -97,24 +37,13 @@ function removeMovieFromDOM(thisMovieTmdbId){
     }
 }
 
-function clearSearchResults(){
-    $("#moviesearch-input").val('');
-    $("div.carousel").animate({height: "0px"}, 150).html("");
-}
 
 function clearMovieList(){
     movie_list = [];
     $('#movie_list').html("");
 }
 
-//Delay wrapper function (to limit ajax queries when typing)
-function delay(fn, ms) {
-    let timer = 0
-    return function (...args) {
-        clearTimeout(timer)
-        timer = setTimeout(fn.bind(this, ...args), ms || 0)
-    }
-}
+
 
 // Attach handlers to DOM elements
 const init = () => {
@@ -125,29 +54,6 @@ const init = () => {
     
     // Materialize Modal initialize
     $('.modal').modal();
-    //==========================================================================
-    //Prevent normal form behavior for search
-    $('.ajax-form').submit(function(e){
-        e.preventDefault();
-    })
-
-    //==========================================================================
-
-    //Dim movie list when searching
-    $("#moviesearch-input").focus(activateSearch)
-    $(document).click(function(event){
-        let clickedTarget = $(event.target);
-        // If user clicks outside of search container
-        if (!clickedTarget.closest("#search-container").length){
-            deactivateSearch();
-        }
-    })
-
-
-    //Custom autocomplete jquery ajax to materialize carousel feature
-    $('#moviesearch-input').on("input", delay(function () {
-        searchMovies.call(this);
-    }, 1000));
 
     //==========================================================================
 
@@ -171,10 +77,6 @@ const init = () => {
 
     //==========================================================================
     
-    // Clear search results
-    $("#search-close").click(function() {
-        clearSearchResults();
-    });
 
     // Button to clear current movie list FLAG: Is used?
     $("#clear").click(function (){
