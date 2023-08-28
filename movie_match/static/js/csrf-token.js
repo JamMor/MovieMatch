8e05b68cac6af69f04b7a35543339b52a242046d
JamMor <JamMor@gmail.com>
2023-08-28 13:20:59 -0700

    Adjusted csrfToken to not be stored in global var
    Not needed as a var anyway

--- movie_match/static/js/csrf-token.js
+++ movie_match/static/js/csrf-token.js
@@ -19,16 +19,15 @@ function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
     return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
 }
-$.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});
 
 const init = () => {
-    csrftoken = getCookie('csrftoken');
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+            }
+        }
+    });
 }
 
 export {init}
