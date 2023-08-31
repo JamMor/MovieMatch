7dd6e59e09819b49bd53e9ba2b92c9ab48a21369
JamMor <JamMor@gmail.com>
2023-08-31 14:21:02 -0700

    Made user_list a scrollable slider element

--- elimination_room/static/elimination_room/js/index.js
+++ elimination_room/static/elimination_room/js/index.js
@@ -4,6 +4,7 @@ import * as MaterializeComponents from "./materialize-inits.js";
 import {createMatchSocket} from "./sockets.js";
 import * as CopyToClipBoard from "./sharecode_copy.js";
 import { newSave } from "/static/js/save_list.js";
+import * as UserSlider from "/static/list_builder/js/list_manager/slider.js"
 
 //Elimination Room Entry Point
 $(document).ready(function() {
@@ -12,4 +13,5 @@ $(document).ready(function() {
     SendCommands.init(matchSocket);
     CopyToClipBoard.init();
     newSave();
+    UserSlider.init();
 });
