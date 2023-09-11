5032c5256332d45e1f007f8a8e17e0701a3f3e92
JamMor <JamMor@gmail.com>
2023-09-11 14:39:48 -0700

    Changed underscore to hypen in url

--- list_builder/urls.py
+++ list_builder/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "list_builder"
 urlpatterns = [
     path('', views.index, name="list_builder"),
-    path('list_manager', views.list_manager, name="list_manager"),
+    path('list-manager', views.list_manager, name="list_manager"),
     path('save/<int:list_id>', views.save, name="save_list"),
     path('save', views.save, name="save_list"),
     path('edit/<int:list_id>', views.edit, name="edit_list"),
