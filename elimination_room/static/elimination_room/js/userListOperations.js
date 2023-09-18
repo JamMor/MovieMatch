dac530fa4b7cd0758ce7cec9438830dc070ff5f2
JamMor <JamMor@gmail.com>
2023-09-18 15:54:36 -0700

    Unimplemented User List class

---
+++ elimination_room/static/elimination_room/js/userListOperations.js
@@ -0,0 +1,224 @@
+import { UserChip } from "/static/js/DOMelements.js";
+
+class UserList {
+    constructor($listDomContainer, selfUUID, users = {}){
+        this.$listDomContainer = $listDomContainer;
+        this.selfUUID = selfUUID;
+        this.users = users;
+    }
+    prefix = "user";
+    
+    isSelf(uuid){
+        return uuid == this.selfUUID;
+    }
+
+    convertNestedToFlat(userObject){
+        const uuid = Object.keys(userObject)[0];
+        const {position, nickname} = userObject[uuid];
+        return {uuid, position, nickname}
+    }
+
+    getUserByUuidFlat(uuid){
+        if(this.users.hasOwnProperty(uuid)){
+            const user = this.users[uuid];
+            user.uuid = uuid;
+            return user;
+        }
+        else{
+            console.log(`ERROR: User ${uuid} not found.`)
+            return null;
+        }
+    }
+
+    getChipHtml({uuid,nickname,position}) {
+        return UserChip(this.prefix, {uuid,nickname,position}, this.isSelf(uuid))
+    }
+
+    get$userChips() {
+        return this.$listDomContainer.children(".chip");
+    }
+
+    uuidFromUserChipDomId(domId) {
+        return domId.split("_")[1];
+    }
+
+    domIdFromUuid(uuid) {
+        return `${this.prefix}_${uuid}`;
+    }
+
+    getUuids() {
+        return Object.keys(this.users)
+    }
+
+    getDomIds() {
+        return this.get$userChips().map(function($chip) {
+            return this.uuidFromUserChipDomId($chip.attr('id'));
+            }, this)
+            .get()
+    }
+
+    updateUser({uuid, position, nickname}) {
+        let changePosition = false;
+        let changeNickname = false;
+        const originalUser = this.getUserByUuidFlat(uuid);
+        if (originalUser.position != position){
+            originalUser.position = position;
+            $(`#${this.domIdFromUuid(uuid)}`).attr('data-position', position);
+            changePosition = true;
+        }
+        if (originalUser.nickname != nickname){
+            originalUser.nickname = nickname;
+            $(`#${this.domIdFromUuid(uuid)}`).text(nickname);
+            changeNickname = true;
+        }
+
+        return {"position:": changePosition, "nickname": changeNickname}
+    }
+
+    addUserToOrderedDOM(chipHtml, position) {
+        // Chip must be inserted at the right position
+        let $userElements = this.get$userChips();
+        // Default to end of list
+        let insertBeforeIndex = $userElements.length;
+
+        // Return the index of the first element with a position greater than the new user's position
+        // Multiple users can have position 0, so they will be ordered by arrival
+        $userElements.each(function(index){
+            let elementPosition = parseInt($(this).attr('data-position'));
+            if (elementPosition > position){
+                insertBeforeIndex = index
+                return false
+            }
+        })
+
+        if (insertBeforeIndex < $userElements.length){
+            $userElements.eq(insertBeforeIndex).before(chipHtml);
+        }
+        else{
+            this.$listDomContainer.append(chipHtml);
+        }
+    }
+
+    addUserToListFlat({uuid,nickname,position}) {
+        if (this.users.hasOwnProperty(uuid)){
+            console.log(`ERROR: User ${uuid} already in list.`)
+            return false;
+        }
+        else{
+            this.users[uuid] = {"position": position, "nickname": nickname};
+            const chipHtml = this.getChipHtml({uuid,nickname,position});
+            this.addUserToOrderedDOM(chipHtml, position);
+            console.log(`${nickname} has joined the room. UUID: ${uuid}`)
+            return true;
+        }
+    }
+
+    addUserToListNested(userObject) {
+        const flatUser = this.convertNestedToFlat(userObject);
+        this.addUserToListFlat(flatUser);
+    }
+
+    removeUserFromListByUuid(uuid) {
+        if (this.users.hasOwnProperty(uuid)){
+            const removedUser = this.getUserByUuidFlat(uuid);
+            delete this.users[uuid];
+            $(`#${this.domIdFromUuid(uuid)}`).remove();
+            return removedUser;
+        }
+        else{
+            console.log(`ERROR: User ${uuid} not found in list.`)
+            return null;
+        }
+    }
+
+    updateDomOrder(){
+        function sortByDataPosition(a, b){
+            return parseInt($(a).attr('data-position')) - parseInt($(b).attr('data-position'))
+        }
+        const $userElements = this.get$userChips();
+        $userElements.sort(sortByDataPosition).appendTo(this.$listDomContainer);
+    }
+
+    syncLists(newList){
+        const currentUuids = new Set(this.getUuids())
+        const newUuids = new Set(Object.keys(newList))
+        
+        //Get the removed users
+        const removedUuids = new Set([...currentUuids].filter(oldUuid => !newUuids.has(oldUuid)))
+        //Get the added users
+        const addedUuids = new Set([...newUuids].filter(newUuid => !currentUuids.has(newUuid)))
+        
+        //Remove users from list and DOM
+        removedUuids.forEach(uuid => this.removeUserFromListByUuid(uuid))
+
+        //Update persisting users with new positions and nicknames
+        // FLAG: Fix newList format when server changed
+        let needReorder = false;
+        this.getUuids().forEach(uuid => {
+            const userObj = this.convertNestedToFlat({[uuid]: newList[uuid]})
+            const {position} = this.updateUser(userObj)
+            if (position == true){
+                needReorder = true;
+            }
+        })
+        if (needReorder){
+            this.updateDomOrder();
+        }
+
+        //Add users to DOM and user_list
+        addedUuids.forEach(uuid => this.addUserToListNested({[uuid]: newList[uuid]}))
+    }
+
+    clearList() {
+        this.users = {};
+        this.$listDomContainer.empty();
+    }
+
+    verifyUserListDOMSync(){
+        const $userElements = this.get$userChips();
+        //Confirming DOM and user list are in sync FLAG
+        const domUsers = $userElements.map(function() {
+            const thisUuid = this.uuidFromUserChipDomId(this.id);
+            const thisPosition = parseInt($(this).attr('data-position'));
+            const thisNickname = $(this).text().trim();
+            return {"uuid": thisUuid, "position": thisPosition, "nickname": thisNickname} ;
+            })
+            .get()
+    
+        let errorMessage;
+        const errorMessages = {
+            "length": "DOM and user list are not the same length.",
+            "missing_uuid": "DOM has a user that is not in user list.",
+            "position": "Positions not synced.",
+            "nickname": "Nicknames not synced."
+        }
+    
+        const sourceLength = this.getUuids().length;
+        if(domUsers.length == sourceLength){
+            for(let domUser of domUsers){
+                if(!this.users.hasOwnProperty(domUser.uuid)){
+                    errorMessage = errorMessages.missing_uuid;
+                    break;
+                }
+                if(this.users[domUser.uuid].position != domUser.position){
+                    errorMessage = errorMessages.position;
+                    break;
+                }
+                if(this.users[domUser.uuid].nickname != domUser.nickname){
+                    errorMessage = errorMessages.nickname;
+                    break;
+                }
+            }
+        }
+        else {
+            errorMessage = errorMessages.length
+        }
+        
+        console.log(`DOM and user list are${errorMessage ? " NOT" : ""} in sync.`)
+        if (errorMessage){
+            console.log(errorMessage)
+        }
+    }
+}
+
+export { UserList }
