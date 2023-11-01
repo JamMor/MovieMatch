fcd366416c18ee26c5dd38825ffde11e3899fa64
JamMor <JamMor@gmail.com>
2023-11-01 14:37:04 -0700

    Removed obsolete (and wrong) form_error method
    Form.errors dictionary can just be added directly, or use the new
    'combine' method

--- movie_match/json_response_models.py
+++ movie_match/json_response_models.py
@@ -43,19 +43,6 @@ class FailedFormResponse(FailedJsonClassObject):
                 form_errors = {}
         self.form_errors = form_errors
     
-    def add_form_errors(self, form_error_list:dict[str,list[dict]]):
-        """
-        Gets a Django error dictionary and reformats it to only be a list of error
-        messages for each field (key).
-
-        :param form_error_list: A dictionary of errors as returned from the API.
-        :return: A dictionary of errors in a more readable format.
-        """
-        error_dict = {}
-        for field, error_list in form_error_list.items():
-            error_dict[field] = [error['message'] for error in error_list]
-        self.form_errors.update(error_dict)
-
     def clear_form_errors(self):
         """Clears the form_errors property."""
         self.form_errors = {}
